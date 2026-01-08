1835e582477512f6e1003eff7cbf9b1d79d0b4f9
greatit-lab <gil2zum@gmail.com>
2026-01-08 19:08:29 +0900

    Create lamplife.module.ts --0108_19

---
+++ src/lamplife/lamplife.module.ts
@@ -0,0 +1,11 @@
+// ITM-DATA-API/src/lamplife/lamplife.module.ts
+import { Module } from '@nestjs/common';
+import { LampLifeController } from './lamplife.controller';
+import { LampLifeService } from './lamplife.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [LampLifeController],
+  providers: [LampLifeService, PrismaService],
+})
+export class LampLifeModule {}
