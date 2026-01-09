a163b535af5cc4e885e838f29cd1fec1e9f62ee1
greatit-lab <gil2zum@gmail.com>
2026-01-09 14:15:45 +0900

    Create dashboard.module.ts --0109_14

---
+++ src/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+// ITM-Data-API/src/dashboard/dashboard.module.ts
+import { Module } from '@nestjs/common';
+import { DashboardController } from './dashboard.controller';
+import { DashboardService } from './dashboard.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [DashboardController],
+  providers: [DashboardService, PrismaService],
+  exports: [DashboardService],
+})
+export class DashboardModule {}
