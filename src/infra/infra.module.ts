471802239b988db891675dc005033cedd24bd439
greatit-lab <gil2zum@gmail.com>
2026-01-09 14:23:07 +0900

    Create infra.module.ts --0109_14

---
+++ src/infra/infra.module.ts
@@ -0,0 +1,11 @@
+// ITM-Data-API/src/infra/infra.module.ts
+import { Module } from '@nestjs/common';
+import { InfraController } from './infra.controller';
+import { InfraService } from './infra.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [InfraController],
+  providers: [InfraService, PrismaService],
+})
+export class InfraModule {}
