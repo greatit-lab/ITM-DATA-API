1f3b8774226b5b49a84b6b3cf6e3e3d8eb6a03dc
greatit-lab <gil2zum@gmail.com>
2026-01-09 11:41:38 +0900

    Create admin.module.ts --0109_11

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,11 @@
+// ITM-Data-API/src/admin/admin.module.ts
+import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [AdminController],
+  providers: [AdminService, PrismaService],
+})
+export class AdminModule {}
