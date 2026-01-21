05aab35eb94fb7ae66d206de47cfd31ca954d455
greatit-lab <gil2zum@gmail.com>
2026-01-21 12:13:14 +0900

    Create BoardModule with controller and services --0121_12

---
+++ src/board/board.module.ts
@@ -0,0 +1,11 @@
+// ITM-Data-API/src/board/board.module.ts
+import { Module } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { BoardController } from './board.controller';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [BoardController],
+  providers: [BoardService, PrismaService],
+})
+export class BoardModule {}
