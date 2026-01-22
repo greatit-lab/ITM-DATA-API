1b18c7759805a7368dc4774d0418efe94481b6f2
greatit-lab <gil2zum@gmail.com>
2026-01-22 14:56:27 +0900

    Update board.module.ts --0122_14

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { BoardController } from './board.controller';
 import { PrismaService } from '../prisma.service';
+import { AlertModule } from '../alert/alert.module'; // [추가]
 
 @Module({
+  imports: [AlertModule], // [추가] AlertService 주입을 위해 필요
   controllers: [BoardController],
   providers: [BoardService, PrismaService],
 })
