1f180f4eb0cd4e1e087b108cb2d1c057120d042c
greatit-lab <gil2zum@gmail.com>
2026-01-27 15:11:13 +0900

    Update alert.module.ts --0127_15

--- src/alert/alert.module.ts
+++ src/alert/alert.module.ts
@@ -7,6 +7,6 @@ import { PrismaService } from '../prisma.service';
 @Module({
   controllers: [AlertController],
   providers: [AlertService, PrismaService],
-  exports: [AlertService], // BoardModule에서 사용하기 위해 export
+  exports: [AlertService], // 다른 모듈(Board 등)에서 알림 생성 시 사용
 })
 export class AlertModule {}
